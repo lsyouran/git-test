6c93ada40702ad498e7a1d267397a82d0d8cb9db
lsyouran <72910195+lsyouran@users.noreply.github.com>
2021-04-28 17:06:59 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git5");
         System.out.println("hello git6");
         System.out.println("hello git8");
+        System.out.println("hello test");
 
     }
 }
